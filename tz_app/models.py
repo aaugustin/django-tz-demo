32f5e696f412981e5178461667fa04e1e2ce2bdc
Aymeric Augustin <aymeric.augustin@m4x.org>
2011-10-31 11:32:54 +0100

    Added __unicode__ methods for models.

--- tz_app/models.py
+++ tz_app/models.py
@@ -4,7 +4,13 @@ class Name(models.Model):
     name = models.CharField(max_length=32, unique=True)
     dated = models.DateTimeField()
 
+    def __unicode__(self):
+        return self.name
+
 class AutoName(models.Model):
     name = models.CharField(max_length=32, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
