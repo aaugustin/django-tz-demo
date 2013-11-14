8d90be269ab5ac1c627134590bbcfc96b55c4ee0
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-11-14 14:24:33 +0100

    Clarified what's shown in the tables.

--- tz_app/context_processors.py
+++ tz_app/context_processors.py
@@ -8,13 +8,13 @@ except ImportError:
     pytz = None
 
 def timezones(request):
-    alt_timezone = request.session.get('alt_timezone', (pytz or timezone).utc)
-    if pytz is not None and isinstance(alt_timezone, six.text_type):
-        alt_timezone = pytz.timezone(alt_timezone)
+    other_timezone = request.session.get('other_timezone', (pytz or timezone).utc)
+    if pytz is not None and isinstance(other_timezone, six.text_type):
+        other_timezone = pytz.timezone(other_timezone)
     return {
         'pytz': pytz,
         'default_timezone_name': settings.TIME_ZONE,
         'timezones': pytz.common_timezones if pytz else [],
-        'alt_timezone': alt_timezone if pytz else timezone.utc,
-        'alt_timezone_name': alt_timezone.zone if pytz else 'UTC',
+        'other_timezone': other_timezone if pytz else timezone.utc,
+        'other_timezone_name': other_timezone.zone if pytz else 'UTC',
     }
--- tz_demo/urls.py
+++ tz_demo/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('tz_app.views',
     url(r'^$', 'home', name='home'),
     url(r'^timezone/', 'set_timezone', name='set_timezone'),
-    url(r'^alt_timezone/', 'set_timezone', name='set_alt_timezone',
-                kwargs={'key': 'alt_timezone', 'name': 'alternative'}),
+    url(r'^other_timezone/', 'set_timezone', name='set_other_timezone',
+                kwargs={'key': 'other_timezone', 'name': 'alternative'}),
     url(r'^locale/', 'set_locale', name='set_locale'),
 )
 
